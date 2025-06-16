78b5ac4860495ef96c232b84401860f137d97cf9
Claudia Pellegrino <claui@users.noreply.github.com>
2025-06-16 20:18:16 +0200

    ci: set up locales for unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,5 +2,5 @@
 
 import locale
 
-locale.setlocale(locale.LC_MONETARY, 'de_DE')
-locale.setlocale(locale.LC_TIME, 'de_DE')
+locale.setlocale(locale.LC_MONETARY, 'de_DE.UTF-8')
+locale.setlocale(locale.LC_TIME, 'de_DE.UTF-8')
